e654a7af9625f279ca25c139c14ba5b8f2a9e3ab
Reena <reena.sanjay.sharma@gmail.com>
2018-10-29 21:56:47 +0000

    can see image on right side

--- src/Components/MovieListing/MovieListing.js
+++ src/Components/MovieListing/MovieListing.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import FetchMovieList from '../Api.js';
+import './MovieListing.css'
 
 class MovieListing extends Component {
   state = {
-    movies: []
+    movies: [],
+    posterUrl: ""
   }
 
   getMovies = async (title) => {
     const movieList = await FetchMovieList(title);
-    this.setState({ movies: movieList});
+    const poster = (movieList.length > 0) ? movieList[0].Poster : "";
+    this.setState({movies: movieList, posterUrl: poster})
   }
 
   componentDidMount = async () => {
@@ -20,18 +23,31 @@ class MovieListing extends Component {
     await this.getMovies(title);
   }
 
+  showDetails = (event) => {
+    let item = event.currentTarget.id
+    this.setState({posterUrl:this.state.movies[item].Poster})
+  }
+
   render() {
-    const{ movies} = this.state;
-  
+    const{ movies, posterUrl } = this.state;
+    console.log(movies , posterUrl);
+    
     return(
-      <div>
-        <input className="SearchInput" type="text" onChange={this.handleSearch} />
-        <div> {movies.map((movie, index) =>
-          <span key={movie.imdbID} id={index}>
-          <p>{movie.Title}</p>
-          <img src={movie.Poster} alt={movie.Title} />
-          </span> )} 
-        </div>
+      <div className="Container" >
+        <div className="List">
+          <div className="MovieList"> {movies.map((movie, index) =>
+            <div className="Movie" key={movie.imdbID} id={index}  onClick={this.showDetails} >
+            <p className="MovieTitle">{movie.Title}</p>
+            <img  className="LeftHandPoster" src={movie.Poster} alt={movie.Title} />
+            </div> )} 
+          </div>
+          <div className="SearchInput">
+          <input type="text" onChange={this.handleSearch} placeholder="Enter movie title" />
+          </div>
+          </div>
+          <div className="Poster">
+        <img className="RightHandPoster"src={posterUrl} alt=""/>
+        </div> 
       </div>
     )
   }
